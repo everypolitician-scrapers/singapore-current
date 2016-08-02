3c584d2ccbea7c8baf1d6f35ace7f08e3517dfda
Tony Bowden <tony@tmtm.com>
2016-08-02 10:00:13 +0100

    Follow links to MP pages

--- scraper.rb
+++ scraper.rb
@@ -5,9 +5,11 @@ require 'nokogiri'
 require 'pry'
 require 'scraperwiki'
 
+# require 'open-uri/cached'
 require 'scraped_page_archive/open-uri'
 
 def noko_for(url)
+  # warn "Fetching #{url}"
   Nokogiri::HTML(open(url).read)
 end
 
@@ -15,5 +17,11 @@ end
 
 _ = noko_for('http://www.parliament.gov.sg/list-constituencies')
 _ = noko_for('http://www.parliament.gov.sg/list-ncmps-and-nmps')
-_ = noko_for('http://www.parliament.gov.sg/list-of-current-mps')
+
+
+LIST_PAGE = 'http://www.parliament.gov.sg/list-of-current-mps'
+noko = noko_for(LIST_PAGE)
+noko.css('.view-list-of-mps-current-mps table a[href*="/mp/"]/@href').each do |href|
+  _ = noko_for( URI.join(LIST_PAGE, href.text) )
+end
 
